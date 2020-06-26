4033c836b38049bb573cf148f49964940a5d301e
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-06-26 20:15:23 +0100

    Add logger middleware

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    status: "active",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    status: "inactive",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "active",
+    email: "Nathan@yesenia.net",
+  },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const path = require("path");
+const members = require("./members");
+const logger = require("./middleware/logger");
 
 const app = express();
+
+// Init middleware
+app.use(logger);
+
+// Gets All Members
+app.get("/api/members", (req, res) => res.json(members));
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, "public")));
