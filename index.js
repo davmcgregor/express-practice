327c18e03e279e3294485d8f655a7ea16edb9377
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-06-27 13:32:12 +0100

    Add routes directory

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const express = require("express");
 const path = require("path");
-const members = require("./members");
-// const logger = require("./middleware/logger");
+const logger = require("./middleware/logger");
 
 const app = express();
 
 // Init middleware
 // app.use(logger);
 
-// Gets All Members
-app.get("/api/members", (req, res) => res.json(members));
-
-// Get Single Member
-app.get("/api/members/:id", (req, res) => {
-  const found = members.some((member) => member.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
-  }
-});
-
 // Set a static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
